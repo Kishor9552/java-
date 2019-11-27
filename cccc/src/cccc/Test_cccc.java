b606001939f677ca3c29365c9271e596e32fc94c
Shree <Shree@192.168.1.223>
2019-11-27 10:22:30 +0530

    adding addition method

--- cccc/src/cccc/Test_cccc.java
+++ cccc/src/cccc/Test_cccc.java
@@ -5,8 +5,18 @@ public class Test_cccc {
 	public static void main(String[] args) {
 		System.out.println("eclipse massege");
 		System.out.println("github massege");
+		Test_cccc tc=new Test_cccc();
+		tc.addition();
+		
 		
 
+	}
+	public static int addition() {
+		int a=10;
+		int b=20;
+		int sum=a+b;
+		System.out.println(sum);
+		return sum;
 	}
 
 }
